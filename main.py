bdd18869e76636ba49ddc5e1516032588d85c4a0
Michael Rodriguez Rios <michael_rios05@hotmail.com>
2024-06-04 21:33:32 -0500

    Virtualenv

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from PIL import Image
-import io
+import requests
+from io import BytesIO
+import cv2
 import numpy as np
 
 app = FastAPI()
@@ -21,28 +23,27 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 # Cargar el modelo
-with tf.keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
-    modelo_cargado = load_model('mm/modelo_entrenado.h5')
+with tf.keras.utils.custom_object_scope({"KerasLayer": hub.KerasLayer}):
+    # modelo_cargado = tf.keras.models.load_model("mm/modelo_entrenado.h5")
+    # modelo_cargado = tf.keras.models.load_model('mm/modelo_entrenado.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+    modelo_cargado = load_model("mm/modelo_entrenado.h5")
+
 
 class ImagePredictionResponse(BaseModel):
     prediction: str
 
 
-def preprocess_image(image: Image.Image) -> np.ndarray:
-    image = image.resize((224, 224))
-    image_array = np.array(image) / 255.0  # Normaliza los valores de píxel
-    image_array = np.expand_dims(image_array, axis=0)
-    return image_array
-
 @app.post("/predecir-imagen", response_model=ImagePredictionResponse)
 async def predict_image(file: UploadFile = File(...)):
     contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
-    processed_image = preprocess_image(image)
+    respuesta = requests.get(contents)
+    img = Image.open(BytesIO(respuesta.content))
+    img = np.array(img).astype(float) / 255
 
-    # Realiza la predicción
-    prediction = modelo_cargado.predict(processed_image)
+    img = cv2.resize(img, (224, 224))
+    prediction = modelo_cargado.predict(img.reshape(-1, 224, 224, 3))
     emotion = np.argmax(prediction)
 
     # Emoción correspondiente
@@ -53,7 +54,7 @@ async def predict_image(file: UploadFile = File(...)):
         "neutral",
         "sorprendido",
         "temeroso",
-        "triste"
+        "triste",
     ]
     predicted_emotion = emotion_labels[emotion]
 
