cf8e0f59a6f3f0467ae7b2cd4c4d0871503b46ec
Michael Rodriguez Rios <michael_rios05@hotmail.com>
2024-06-06 13:58:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from typing import Union
 from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 from PIL import Image
 import requests
 from io import BytesIO
@@ -13,6 +11,11 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tf_keras as tfk
 from tensorflow.keras.models import load_model
+
+# Cargar el modelo
+with tf.keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
+    #modelo_cargado = load_model('mm/modelo_entrenado.keras')
+    modelo_cargado = load_model('mm/modelo_entrenado.keras')
 
 app = FastAPI()
 
@@ -25,10 +28,14 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-# Cargar el modelo
-#with tf.keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
-    modelo_cargado = load_model('mm/modelo_entrenado.h5')
+def categorizar(url):
+  respuesta = requests.get(url)
+  img = Image.open(BytesIO(respuesta.content))
+  img = np.array(img).astype(float)/255
 
+  img = cv2.resize(img, (224,224))
+  prediccion = modelo_cargado.predict(img.reshape(-1, 224, 224, 3))
+  return np.argmax(prediccion[0], axis=-1), prediccion
 
 class ImagePredictionResponse(BaseModel):
     prediction: str
